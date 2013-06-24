8a56dccd6ebd4ac811d633a3999de8c0d9d6d0f1
Eric W. Brown <feneric@gmail.com>
2013-06-24 19:13:28 -0400

    Created test "gold standard" generator script.

---
+++ doxypypy/test/make_samples.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# This script will recreate all the "gold standard" output files based on
+# a run of the existing software. DO NOT RUN THIS UNLESS YOU ARE WILLING
+# TO MANUALLY INSPECT ALL OUTPUT FILES AFTER EXECUTION. FAILING TO DO SO
+# WILL MAKE THE RELEVANT TESTS USELESS.
+
+for sample in sample_*.py;do
+	case $sample in
+		*.out*.py)	:;;
+		*)	sampleBase=`basename -s .py $sample`
+			echo "Processing" $sampleBase
+			../doxypypy.py --autocode --autobrief --ns=$sampleBase $sample > $sampleBase.out.py
+			../doxypypy.py --autocode --autobrief $sample > $sampleBase.outnn.py
+			../doxypypy.py --autobrief --ns=$sampleBase $sample > $sampleBase.outnc.p
+			../doxypypy.py $sample > $sampleBase.outbare.py
+	esac
+done
+
+echo "Processing complete."
+echo
+echo "Now you must visually inspect each output file and verify that"
+echo "it will properly serve as a new gold standard for future"
+echo "comparisons. Failing to do so will make all the comparison"
+echo "tests a waste of time."
+
